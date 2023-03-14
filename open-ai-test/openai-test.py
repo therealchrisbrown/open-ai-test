96ecdad6d02c9f017ade39540da72f68455bae79
therealchrisbrown <ch.braun1@gmx.de>
2023-03-14 18:46:34 +0100

    test

--- open-ai-test/openai-test.py
+++ open-ai-test/openai-test.py
@@ -2,14 +2,18 @@ from decouple import config
 
 OPEN_AI_API_KEY=config('OPENAI_API_KEY')
 
+import os
 import openai
 openai.api_key=OPEN_AI_API_KEY
 
-response = openai.Completion.create(
-  engine="text-davinci-003",
-  prompt="Ich würde gerne einen Flug buchen",
-  max_tokens=60
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
 )
 
-answer = response.choices[0].text
-print(answer)
+print(completion)
